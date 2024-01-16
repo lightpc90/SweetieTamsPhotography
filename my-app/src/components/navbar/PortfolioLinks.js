943be55ccae0b08c87377df48c49752e1ee62ef8
lightpc90 <afoageektech@gmail.com>
2024-01-16 22:21:55 +0100

    corrected remotes and trainig routes

--- my-app/src/components/navbar/PortfolioLinks.js
+++ my-app/src/components/navbar/PortfolioLinks.js
@@ -3,12 +3,12 @@ import React from 'react'
 
 const PortfolioLinks = () => {
   return (
-      <div className='flex flex-col gap-3 p-5 justify-center items-center font-semibold lg:text-xl'>
+      <div className='flex flex-col gap-3 p-3 justify-center items-center font-semibold lg:text-xl'>
           <Link href='/portfolio/portraits'>Portraits</Link>
           <Link href='/portfolio/fashions'>Fashions</Link>
           <Link href='/portfolio/events'>Events</Link>
           <Link href='/portfolio/remotes'>Remotes</Link>
-          <Link href='/porfolio/training'>Training</Link>
+          <Link href='/portfolio/training'>Training</Link>
     </div>
   )
 }
