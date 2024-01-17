0bb545c99a3b5447c7ac5a7f6da87c8f5767799b
lightpc90 <afoageektech@gmail.com>
2024-01-17 01:18:17 +0100

    added local pictures and imported for fashions

--- my-app/src/components/portfolio/fashions/Fashions.js
+++ my-app/src/components/portfolio/fashions/Fashions.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { fashions } from '@/data/portfolio/fashions/Fashions'
+import Image from 'next/image'
 
 const Fashions = () => {
   return (
-      <div>
-          <div>Fashions</div>
+    <div>
+      <div className="font-bold text-5xl lg:text-7xl text-center">Fashions</div>
+      <div className="flex flex-wrap justify-center items-center gap-3 my-10">
+        {fashions.map((img, i) => (
+            <Image src={img} alt={`${img}`} width={600} height={600} key={i} className='w-10/12 lg:w-4/12 shadow-md'/>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
 export default Fashions
---
+++ my-app/src/data/portfolio/fashions/Fashions.js
@@ -0,0 +1,17 @@
+// import all pictures for fashions
+
+
+import img1 from "../../../../public/Fashion/_MG_0093.jpg";
+import img2 from "../../../../public/Fashion/_MG_0121.jpg";
+import img3 from "../../../../public/Fashion/DEX 4.jpg";
+import img4 from "../../../../public/Fashion/ES5.jpg";
+import img5 from "../../../../public/Fashion/G1.jpg";
+import img6 from "../../../../public/Fashion/G2.jpg";
+import img7 from "../../../../public/Fashion/G3.jpg";
+import img8 from "../../../../public/Fashion/LFW1.jpg";
+import img9 from "../../../../public/Fashion/LFW19.jpg";
+import img10 from "../../../../public/Fashion/LFW33.jpg";
+import img11 from "../../../../public/Fashion/OP1.jpg";
+import img12 from "../../../../public/Fashion/TO5.jpg";
+
+export const fashions = [img1, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img2]
