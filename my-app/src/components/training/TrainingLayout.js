8b9f20d4a693b324364c63aed4b4e3069f2edae4
lightpc90 <afoageektech@gmail.com>
2024-02-01 18:56:21 +0100

    added key props to TrainingLayout

--- my-app/src/components/training/TrainingLayout.js
+++ my-app/src/components/training/TrainingLayout.js
@@ -1,13 +1,16 @@
-import React from 'react'
+'use client'
+
+import {useState} from 'react'
 import Subtopics from './Subtopics';
 
-const TrainingLayout = ({training}) => {
+const TrainingLayout = ({ training }) => {
+    const [topicOpened, setTopicOpened] = useState(false)
     return (
       <div className='p-3'>
         <h4 className='font-bold'>{`${training.duration} Training`}</h4>
         <div>
           {training.topics.map((eachTopic, i) => (
-            <section>
+            <section key={i}>
                   <h4 className='font-bold text-gray-500 py-2'>{`Week ${i + 1}: ${eachTopic.topic}`}</h4>
                   
               <Subtopics key={i} eachTopic={eachTopic} />
