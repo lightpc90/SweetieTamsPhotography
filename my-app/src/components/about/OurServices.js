e9d24e8c981be7fd8d0ffbbb4e6daa25e2ed090f
lightpc90 <afoageektech@gmail.com>
2024-02-01 15:38:52 +0100

    added google business link added about me Intro to OurServices component

--- my-app/src/components/about/OurServices.js
+++ my-app/src/components/about/OurServices.js
@@ -2,11 +2,36 @@ import React from 'react'
 
 const OurServices = () => {
   return (
-      <div>
-          <div className='mt-4 font-bold text-xl lg:text-2xl'>Our Services</div>
-          <div> </div>
+    <div>
+      <div className="flex flex-col mt-4 p-5 lg:text-2xl">
+
+        <p>
+          Creating beautiful stories is my way of life. This is why photography
+          for me has gone beyond just clicking the shutter button! Your story is
+          my priority.
+        </p>
+        <p>
+          So whenever I am given the opportunity to tell your story, I give it
+          my all by:
+        </p>
+        <ul className='font-semibold pl-7'>
+          <li>— Delivering high quality images best suit your vision </li>
+          <li> — Communicating effectively</li>
+          <li>— Suggesting creative ideas that </li>
+          <li> — Making you feel relaxed in front of the camera</li>
+          <li>
+            — Suggesting different poses that align with your personality{" "}
+          </li>
+          <li>— Choosing the perfect location. </li>
+        </ul>
+        <p>
+          If you are looking for a portrait or virtual (remote) photographer who
+          combines technical expertise with creative vision, I would love to
+          hear from you. Let's tell your story together!
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default OurServices
--- my-app/src/components/testimonies/Testimonies.js
+++ my-app/src/components/testimonies/Testimonies.js
@@ -4,11 +4,13 @@ import Link from 'next/link';
 
 const Testimonies = () => {
   return (
-    <div className='flex w-full flex-col justify-center items-center my-10'>
+    <div className="flex w-full flex-col justify-center items-center my-10">
       <p className="text-gray-400">Reviews From Our Costumers</p>
       <p className="font-bold text-xl lg:text-3xl text-center">Testimonies</p>
       <TestimoniesSlide />
-      <Link href='/' className='font-semibold'>You Can Check all Our Reviews Here</Link>
+      <Link href="https://g.co/kgs/xCsQLYp" target='_blank' rel='noopener noreferer' className="font-semibold">
+        Check Our Reviews on Google Business
+      </Link>
     </div>
   );
 }
