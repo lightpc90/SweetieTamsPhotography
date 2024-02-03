b8d54c4232273e979233274c310f98f77baaef0f
lightpc90 <afoageektech@gmail.com>
2024-02-03 01:11:18 +0100

    added collapse funtionality to the topics

--- my-app/src/components/training/Subtopics.js
+++ my-app/src/components/training/Subtopics.js
@@ -1,13 +1,26 @@
-import React from 'react'
+'use client'
 
-const Subtopics = ({eachTopic}) => {
+import {useState} from 'react'
+
+const Subtopics = ({ eachTopic, i }) => {
+  const [topicOpened, setTopicOpened] = useState(false)
   return (
-      <div className='pl-2'>
+    <div className="pl-2 lg:pl-6">
+      <h4
+        onClick={() => {
+          setTopicOpened(!topicOpened);
+        }}
+        className="font-bold text-gray-500 py-2"
+      >{`Week ${i + 1}: ${eachTopic.topic}`}</h4>
+      {topicOpened && (
+        <h4>
           {eachTopic.subtopics.map((subtopic, i) => (
-              <p key={i}>{`- ${subtopic}` }</p>
+            <p key={i}>{`- ${subtopic}`}</p>
           ))}
+        </h4>
+      )}
     </div>
-  )
+  );
 }
 
 export default Subtopics
--- my-app/src/components/training/TrainingLayout.js
+++ my-app/src/components/training/TrainingLayout.js
@@ -1,23 +1,18 @@
-'use client'
 
-import {useState} from 'react'
 import Subtopics from './Subtopics';
 
 const TrainingLayout = ({ training }) => {
-    const [topicOpened, setTopicOpened] = useState(false)
     return (
-      <div className='p-3'>
-        <h4 className='font-bold'>{`${training.duration} Training`}</h4>
+      <div className="p-3">
+        <h4 className="font-bold">{`${training.duration} Training`}</h4>
         <div>
           {training.topics.map((eachTopic, i) => (
             <section key={i}>
-                  <h4 className='font-bold text-gray-500 py-2'>{`Week ${i + 1}: ${eachTopic.topic}`}</h4>
-                  
-              <Subtopics key={i} eachTopic={eachTopic} />
+               <Subtopics key={i} eachTopic={eachTopic} i={i} />
             </section>
           ))}
         </div>
-        <h2 className='font-semibold'>Fee</h2>
+        <h2 className="font-semibold">Fee</h2>
         <p>{`- Fee for the cass = ${training.fee}`}</p>
         <p>{`- Duration = ${training.duration}`}</p>
       </div>
