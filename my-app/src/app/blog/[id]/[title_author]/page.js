268a98d551600463517a44a28c5b05140efc8bf1
lightpc90 <afoageektech@gmail.com>
2024-02-07 11:53:17 +0100

    added link to post page to take users to posts page

--- my-app/src/app/blog/[id]/[title_author]/page.js
+++ my-app/src/app/blog/[id]/[title_author]/page.js
@@ -4,7 +4,7 @@ import BlogPost from '@/components/blog/BlogPost'
 
 const Page = ({ params }) => {
   console.log('params: ', params)
-  const { id, title_author } = params
+  const { id } = params
   const  post = blogPosts[id - 1] || {}
   return (
     <div className='py-10 md:px-[100px] lg:px-[500px]'>
--- my-app/src/components/blog/BlogPost.js
+++ my-app/src/components/blog/BlogPost.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import { dateFormatToNow } from '../utility/dateFormat';
+import Link from 'next/link';
 
 const BlogPost = ({post}) => {
   return (
-    <div className="p-5 md:px-[100px] lg:px-[200px] mb-6 bg-white shadow">
-      {post?.img && <img src={post.img} alt={post.title} />}
-      <h2 className="text-2xl lg:text-3xl font-bold mb-10">{post.title}</h2>
+    <div>
+      <div className="p-5 md:px-[100px] lg:px-[200px] mb-6 bg-white shadow">
+        {post?.img && <img src={post.img} alt={post.title} />}
+        <h2 className="text-2xl lg:text-3xl font-bold mb-10">{post.title}</h2>
 
-      <hr />
-      <div className='my-3'>
-        <p>Written by: sweetie tams</p>
-        <small>{dateFormatToNow(post.date)} </small>
+        <hr />
+        <div className="my-3">
+          <p>Written by: sweetie tams</p>
+          <small>{dateFormatToNow(post.date)} </small>
+        </div>
+        <hr />
+
+        <p className="my-10">{post.content}</p>
       </div>
-      <hr />
-
-      <p className="my-10">{post.content}</p>
+      <Link className='flex justify-center font-bold text-green-900' href='/blog'>Check out other stories</Link>
     </div>
   );
 }
